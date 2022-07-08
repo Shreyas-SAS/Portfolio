e502ca22e31a61b0fe35cf23ed9fa564eb1ed3da
shreyas-SAS <shreyassukhadeve@gmail.com>
2022-07-09 04:01:10 +0530

    updated footer section

--- js/component/footer.js
+++ js/component/footer.js
@@ -5,14 +5,31 @@ window.document.write(`
                 d="M0,96L48,90.7C96,85,192,75,288,112C384,149,480,235,576,256C672,277,768,235,864,186.7C960,139,1056,85,1152,80C1248,75,1344,117,1392,138.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
             </path>
         </svg>
+
         <div class="py-5 bg-main text-center text-white">
             <!--main content of footer-->
-            <p>
-                Made with <i class="fa fa-heart text-danger px-2"></i> by our
-                <a href="https://github.com/learn-opensource" class="text-white text-decoration-none" target="_blank">
-                    team of contributors <i class=" px-2 fa fa-users text-white"></i>
+            <div class="container my-auto" data-aos="fade-in" data-aos-delay="100">
+                <h3 class="display-3 font-weight-light" data-aos="fade-in" data-aos-delay="100"> What Next<span class="pl-1">?</span></h3>
+                <p>
+                    Lets Connect, <br> I am currently open to any new internship opportunities or collaborations in Open Source. <br>
+                    My Inbox is always open, Feel free to drop in a message! 
+                </p>
+            </div>
+
+            <div class="container my-auto">
+                <a href="mailto:20cs01056@iitbbs.ac.in" class="text-dark" target="_blank">
+                    <i class="fa fa-envelope fa-2x px-2"></i>
                 </a>
-            </p>
+                <a href="https://wa.me/918208487623" class="text-dark" target="_blank">
+                    <i class="fa fa-whatsapp fa-2x px-2"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/shreyas-sukhadeve/" class="text-dark" target="_blank">
+                    <i class="fa fa-linkedin fa-2x px-2"></i>
+                </a>
+                <a href="https://github.com/Shreyas-SAS/" class="text-dark" target="_blank">
+                    <i class="fa fa-github fa-2x px-2"></i>
+                </a>
+            </div>
         </div>
     </footer>
 `);
