1b7d0562d0e8a0114dcf25c7055d595516c177b8
shreyas-SAS <shreyassukhadeve@gmail.com>
2022-07-09 06:18:43 +0530

    added major projects section

--- js/component/projects.js
+++ js/component/projects.js
@@ -1,98 +1,79 @@
 window.document.write(`
     <br>
     <div class="container mt-5" data-aos="fade-up">
-        <h1 class="my-3 mb-1 text-muted display-light">Why OpenSource <span class="pl-1">?</span></h1>
-        <p>
-            Contributing to open source has a lot of benefits that just don’t seem to be
-            talked about enough.
-        </p>
+        <h1 class="my-3 mb-1 text-muted display-light">My Projects</h1>
+        
+        <h3 class="my-3 mb-1 text-muted display-light">Major Projects</h3> 
         <div class="row mt-2">
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                <div class="px-3 px-md-5 py-3 m-2 shadow rounded-lg">
-                    <div class="icon mb-2">
-                        <img src="./img/benefits/web-coding.svg" height="65" width="auto" class="center">
+                <div class="px-3 py-3 m-2 shadow rounded-lg">
+                    <div class="icon mb-1">
+                        <img src="./images/internship.png" width="300" height="200" class="center">
                     </div>
-                    <h4 class="text-decoration-none">Code more, learn more!</h4>
-                    <p class="mt-4">
-                        The best way to learn programming is to do more programming!
-                        The second best way is to watch how others do it.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
-                data-aos-delay="200">
-                <div class="px-3 px-md-5 py-3 m-2 shadow rounded-lg">
-                    <div class="icon mb-2">
-                        <img src="./img/benefits/pay.svg" height="65" width="auto" class="center">
-                    </div>
-                    <h4 class="text-decoration-none">Pay back to the community </h4>
-                    <p class="mt-4">
-                        By working on a project used by your community you’re improving the quality of
-                        work for everyone. </p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in"
-                data-aos-delay="300">
-                <div class="px-3 px-md-5 py-3 m-2 shadow rounded-lg">
-                    <div class="icon mb-2">
-                        <img src="./img/benefits/communication.svg" height="65" width="auto" class="center">
-                    </div>
-                    <h4 class="text-decoration-none">
-                        Reputation and can leverage your career
-                    </h4>
-                    <p class="mt-4">
-                        Active open source contributions emphasize your expertise and knowledge, more
-                        than certificates alone will ever do.
+                    <h4 class="text-decoration-none">MediGeniX</h4>
+                    <p class="mt-2">
+                    Symptom based Disease Prediction Model to predict Critical Diseases using symptom and report values by high accuracy ML Regression models hosted as a Webapp.
                     </p>
+
+                    <a href="https://shreyas-sas-medigenix-medigenix-fvafo5.streamlitapp.com/" target="_blank" data-aos-delay="800" data-aos="fade-bottom">
+                        <button
+                            class="btn btn-primary shadow text-decoration-none btn-link px-3 py-1 mt-2 mr-2 rounded-0 border-0 text-white bg-main">
+                            <b>Deployed</b>
+                        </button>
+                    </a>
+
+                    <a href="https://github.com/Shreyas-SAS/Medigenix" target="_blank" data-aos-delay="800" data-aos="fade-bottom">
+                        <button
+                            class="btn btn-primary shadow text-decoration-none btn-link px-3 py-1 mt-2 ml-2 rounded-0 border-0 text-white bg-main">
+                            <b>Github</b>
+                        </button>
+                    </a>
+
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
-                <div class="px-3 px-md-5 py-3 m-2 shadow rounded-lg">
-                    <div class="icon mb-2">
-                        <img src="./img/benefits/reputation.svg" height="65" width="auto" class="center">
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="px-3 py-3 m-2 shadow rounded-lg">
+                    <div class="icon mb-1">
+                        <img src="./images/internship.png" width="300" height="200" class="center">
                     </div>
-                    <h4 class="text-decoration-none">
-                        Meet great people
-                    </h4>
-                    <p class="mt-4">
-                        Meet the community, exchange ideas, grow as a developer. Plenty of quality
-                        discussions, even end up in starting friendships, or finding a mentor.
+                    <h4 class="text-decoration-none">LinkedIn Clone</h4>
+                    <p class="mt-2">
+                    A functional LinkedIn clone with Messages, Like, Connection request and posts features functional as in real LinkedIn app using API connection.
                     </p>
+
+                    <a href="https://shreyas-sas-medigenix-medigenix-fvafo5.streamlitapp.com/" target="_blank" data-aos-delay="800" data-aos="fade-bottom">
+                        <button
+                            class="btn btn-primary shadow text-decoration-none btn-link px-3 py-1 mt-2 mr-2 rounded-0 border-0 text-white bg-main">
+                            <b>Deployed</b>
+                        </button>
+                    </a>
+
+                    <a href="https://github.com/Shreyas-SAS/LinkedIn_Clone" target="_blank" data-aos-delay="800" data-aos="fade-bottom">
+                        <button
+                            class="btn btn-primary shadow text-decoration-none btn-link px-3 py-1 mt-2 ml-2 rounded-0 border-0 text-white bg-main">
+                            <b>Github</b>
+                        </button>
+                    </a>
+
                 </div>
             </div>
 
 
-            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
-                <div class="px-3 px-md-5 py-3 m-2 shadow rounded-lg">
-                    <div class="icon mb-2">
-                        <img src="./img/benefits/transparent.svg" height="65" width="auto" class="center">
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="px-3 py-3 m-2 shadow rounded-lg">
+                    <div class="icon mb-1">
+                        <img src="./images/internship.png" width="300" height="200" class="center">
                     </div>
-                    <h4 class="text-decoration-none">
-                        The Future
-                    </h4>
-                    <p class="mt-4">
-                        Open source is the future. Web, mobile, and cloud solutions are increasingly built
-                        predominantly on open source infrastructure.
+                    <h4 class="text-decoration-none">SuperBill (Ongoing)</h4>
+                    <p class="mt-2">
+                    A complete SuperMarket Website that stores data for new Product entry, their storage position, stock refuling schedule setup, QR based Billing system and Daily revenue summary with periodic GST calculations.
                     </p>
-                </div>
-            </div>
 
-            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
-                <div class="px-3 px-md-5 py-3 m-2 shadow rounded-lg">
-                    <div class="icon mb-2">
-                        <img src="./img/benefits/fun.svg" height="65" width="auto" class="center">
-                    </div>
-                    <h4 class="text-decoration-none">
-                        Fun and gives you personal satisfaction
-                    </h4>
-                    <p class="mt-4">
-                        Last but not least, contributing to open source projects can be a lot of fun, though
-                        challenging at first.
-                    </p>
                 </div>
             </div>
         </div>
+
     </div>
 `);
